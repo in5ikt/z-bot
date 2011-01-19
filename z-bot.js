30be31bfa25c795658036e1279f883cdec7fc6a6
Insikt <in5ikt.github@sekz.se>
2011-01-19 22:41:16 +0000

    Empty
    Signed-off-by: Insikt <in5ikt.github@sekz.se>

--- z-bot.js
+++ z-bot.js
@@ -21,8 +21,8 @@ zBot = {
 	    }
 	}
 	enemyShips = enemyShips.sort( function ( a, b ) {
-	    console.log(a, b);
-	    console.log( typeof a, typeof b );
+	    //console.log(a, b);
+	    //console.log( typeof a, typeof b );
 	    return b.pos.y - a.pos.y;
 	} );
 	return enemyShips;
